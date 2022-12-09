f7cc8ad781e2a60db9406b2ecef05dcac7e16bc4
abdillahnurr <abdillahnurr12@gmail.com>
2022-12-09 14:13:50 +0800

    Fix some css

--- src/scripts/views/pages/about-us.js
+++ src/scripts/views/pages/about-us.js
@@ -1,86 +1,111 @@
 const AboutUs = {
   async render() {
     return ` 
-  <section class="about-team">
-    <div class="team-description">
-      <h1>Team Kami</h1>
-      <p></p> 
-    </div>
-    <div class="container">
-         <div class="row">
-             <div class="section-title">
-                 <h1>Our Team</h1>
-                 <p>These people who work for AnakSehat</p>
-             </div>
-         </div>
-         <div class="row">
-             <div class="team-items">
-                  <div class="item">
-                      <img src="/user/ridho.jpg" alt="team" />
-                      <div class="inner">
-                          <div class="info">
-                               <h5>Abdillah Nur Ridho</h5>
-                               <p>Frontend Developer</p>
-                               <div class="social-links">
-                                   <a href="https://github.com/abdillahnurr"><span class="fa fa-github"></span></a>
-                                   <a href="https://www.instagram.com/abdillahnurr_/"><span class="fa fa-instagram"></span></a>
-                                   <a href="https://www.linkedin.com/in/abdillah-nur-ridho-766285208/"><span class="fa fa-linkedin"></span></a>
-                                   <a href="https://wa.me/085247566970"><span class="fa fa-whatsapp"></span></a>
-                               </div>
-                          </div>
-                      </div>
-                  </div>
-                  <div class="item">
-                    <img src="/user/kynta.jpg" alt="team" />
-                    <div class="inner">
-                        <div class="info">
-                             <h5>Kynta Putri Syakira</h5>
-                             <p>Frontend Developer</p>
-                             <div class="social-links">
-                                 <a href="https://github.com/kyntaputri"><span class="fa fa-github"></span></a>
-                                 <a href="https://www.instagram.com/kyntaputri"><span class="fa fa-instagram"></span></a>
-                                 <a href="https://www.linkedin.com/in/kyntaputri/"><span class="fa fa-linkedin"></span></a>
-                                 <a href="https://wa.me/081347368031"><span class="fa fa-whatsapp"></span></a>
-                             </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="item">
-                    <img src="/user/sultan.jpg" alt="team" />
-                    <div class="inner">
-                        <div class="info">
-                             <h5>Samuel Abelino Simanullang</h5>
-                             <p>Frontend Developer</p>
-                             <div class="social-links">
-                                 <a href=""><span class="fa fa-github"></span></a>
-                                 <a href=""><span class="fa fa-instagram"></span></a>
-                                 <a href=""><span class="fa fa-linkedin"></span></a>
-                                 <a href="https://wa.me/081360885751"><span class="fa fa-whatsapp"></span></a>
-                             </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="item">
-                    <img src="/user/sultan.jpg" alt="team" />
-                    <div class="inner">
-                        <div class="info">
-                             <h5>Sultan Tirta Firdaus</h5>
-                             <p>Frontend Developer</p>
-                             <div class="social-links">
-                                 <a href="https://github.com/sultantirtaa"><span class="fa fa-github"></span></a>
-                                 <a href="https://www.instagram.com/sultantirtaa/"><span class="fa fa-instagram"></span></a>
-                                 <a href="https://www.linkedin.com/in/sultantirta/"><span class="fa fa-linkedin"></span></a>
-                                 <a href="https://wa.me/081283415519"><span class="fa fa-whatsapp"></span></a>
-                             </div>
-                        </div>
-                    </div>
-                </div>
-             </div>
-         </div>
-     </div>
-    </div>
-  </section>
-        `;
+      <section>
+      <div class="row">
+        <h1>Our Team</h1>
+        <p>These people who work for AnakSehat</p>
+      </div>
+      <div class="row">
+        <!-- Column 1-->
+        <div class="column">
+          <div class="card">
+            <div class="img-container">
+              <img src="/user/ridho.jpg" />
+            </div>
+            <h3>Abdillah Nur Ridho</h3>
+            <p>Frontend Developer</p>
+            <div class="icons">
+              <a href="https://www.instagram.com/abdillahnurr_/">
+                <i class="fab fa-instagram"></i>
+              </a>
+              <a href="https://www.linkedin.com/in/abdillah-nur-ridho-766285208/">
+                <i class="fab fa-linkedin"></i>
+              </a>
+              <a href="https://github.com/abdillahnurr">
+                <i class="fab fa-github"></i>
+              </a>
+              <a href="https://wa.me/085247566970">
+                <i class="fab fa-whatsapp"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+        <!-- Column 2-->
+        <div class="column">
+          <div class="card">
+            <div class="img-container">
+              <img src="/user/kynta.jpg" />
+            </div>
+            <h3>Kynta Putri Syakira</h3>
+            <p>Frontend Developer</p>
+            <div class="icons">
+              <a href="https://www.instagram.com/kyntaputri">
+                <i class="fab fa-instagram"></i>
+              </a>
+              <a href="https://www.linkedin.com/in/kyntaputri/">
+                <i class="fab fa-linkedin"></i>
+              </a>
+              <a href="https://github.com/kyntaputri">
+                <i class="fab fa-github"></i>
+              </a>
+              <a href="https://wa.me/081347368031">
+                <i class="fab fa-whatsapp"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+        <!-- Column 3-->
+        <div class="column">
+          <div class="card">
+            <div class="img-container">
+              <img src="/user/sultan.jpg" />
+            </div>
+            <h3>Samuel Abelino Simanullang</h3>
+            <p>Frontend Developer</p>
+            <div class="icons">
+              <a href="https://www.instagram.com/samuelabelino/">
+                <i class="fab fa-instagram"></i>
+              </a>
+              <a href="https://www.linkedin.com/in/samuel-abelino-3b5a93256/">
+                <i class="fab fa-linkedin"></i>
+              </a>
+              <a href="https://github.com/Samuelabelino">
+                <i class="fab fa-github"></i>
+              </a>
+              <a href="https://wa.me/081360885751">
+                <i class="fab fa-whatsapp"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+        <!-- Column 4-->
+        <div class="column">
+          <div class="card">
+            <div class="img-container">
+              <img src="/user/sultan.jpg" />
+            </div>
+            <h3>Sultan Tirta Firdaus</h3>
+            <p>Frontend Developer</p>
+            <div class="icons">
+              <a href="https://www.instagram.com/sultantirtaa/">
+                <i class="fab fa-instagram"></i>
+              </a>
+              <a href="https://www.linkedin.com/in/sultantirta/">
+                <i class="fab fa-linkedin"></i>
+              </a>
+              <a href="https://github.com/sultantirtaa">
+                <i class="fab fa-github"></i>
+              </a>
+              <a href="https://wa.me/081283415519">
+                <i class="fab fa-whatsapp"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+          `;
   },
 };
 
